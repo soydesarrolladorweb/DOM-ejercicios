3341a691a5582b2b1bf4dd4512b979552cf5281f
fabianbarrera <febarrera12@misena.edu.co>
2022-03-08 12:52:23 -0500

    Cambio del evento keyup por input Filtro

--- js/dom/filtro_busquedas.js
+++ js/dom/filtro_busquedas.js
@@ -1,7 +1,7 @@
 const d = document;
 
 export default function searchFilters(input, selector){
-    d.addEventListener("keyup", (e) =>{
+    d.addEventListener("input", (e) =>{
         if(e.target.matches(input)){
             //console.log(e.key);
 
